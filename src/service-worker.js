b6bd77c7363c1fedfa406094241fcb439589c918
Luiz Felipe <102871467+LuizFelipe321@users.noreply.github.com>
2025-09-24 21:35:07 -0300

    Add files via upload

---
+++ src/service-worker.js
@@ -0,0 +1,19 @@
+
+caches.open("app-saudacao-cache").then(cache => {
+  return cache.addAll([
+    "index.html",
+    "manifest.json",
+    "/icon-192.png",
+    "/icon-512.png",
+    "manha.jpg",
+    "tarde.jpg",
+    "noite.jpg",
+    "madrugada.jpg"
+  ]);
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
